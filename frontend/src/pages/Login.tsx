aac8ac46b31f38a95fceb0da90b303a59c1747a2
Sebastien Duvivier <sebastien.duvivier@outlook.fr>
2025-05-16 19:52:21 +0200

    Rename Login.jsx to Login.tsx

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '../components/AuthForm';
+
+const Login: React.FC = () => {
+  return (
+    <div className="auth-page">
+      <h1>Connexion</h1>
+      <AuthForm mode="login" />
+    </div>
+  );
+};
+
+export default Login; 
