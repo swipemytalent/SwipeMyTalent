31610b70c54a159092cafbf62860bee50b275371
nadmax <maximilien.nadji@pm.me>
2025-05-17 11:45:13 +0200

    automate frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    preview: {
+        host: true,
+        port: 8080
+    },
+    build: {
+        outDir: './build',
+        emptyOutDir: true
+    }
 }) 
