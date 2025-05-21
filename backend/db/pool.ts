dd1488a3b95ad2c1dae4ac41e63c1b2b3801c50a
nadmax <maximilien.nadji@pm.me>
2025-05-21 11:33:59 +0200

    setup jwt

--- backend/db/pool.ts
+++ backend/db/pool.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
-import dotenv from 'dotenv';
 
-dotenv.config();
+if (!process.env.POSTGRES_HOST || !process.env.POSTGRES_PORT || !process.env.POSTGRES_USER || !process.env.POSTGRES_PASSWORD || !process.env.POSTGRES_DB) {
+    throw new Error("Missing required PostgreSQL environment variables.");
+}
 
 export const pool = new Pool({
     host: process.env.POSTGRES_HOST,
-    port: 5432,
+    port: parseInt(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
