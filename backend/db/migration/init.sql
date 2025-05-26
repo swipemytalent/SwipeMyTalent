7ca55373979369a0a15292e0f7dd99c20e4d62e0
Maximilien Nadji <maximilien.nadji@pm.me>
2025-05-26 07:37:05 +0200

    update sql users table (#24)

--- backend/db/migration/init.sql
+++ backend/db/migration/init.sql
@@ -1,9 +1,17 @@
-CREATE DATABASE my_database;
-
-\connect my_database;
+SELECT 'CREATE DATABASE my_database'
+WHERE NOT EXISTS (
+  SELECT FROM pg_database WHERE datname = 'my_database'
+)\gexec
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    avatar TEXT,
+    credits INTEGER DEFAULT 0,
+    profile_views INTEGER DEFAULT 0,
+    messages INTEGER DEFAULT 0
 );
