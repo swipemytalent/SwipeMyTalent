bdef8aac09b2e958acebe1f82150b4068a60685a
YoDaMned59 <sduvviier59112@gmail.com>
2025-07-07 14:26:42 +0200

    correct import path for User type from ProfileForm component

--- frontend/src/redux/viewedProfileSlice.ts
+++ frontend/src/redux/viewedProfileSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { User } from '../components/ProfileForm';
+import type { User } from '../components/ProfileForm/ProfileForm';
 
 interface ViewedProfileState {
   value: User | null;
