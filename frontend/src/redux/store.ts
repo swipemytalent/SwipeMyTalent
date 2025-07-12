3e746e3aa6de3498a42b200a8137d6a013910ebc
Sébastien <sduvivier59112@gmail.com>
2025-07-12 11:58:20 +0200

    Revert "forum setup (#85)" (#86)
    This reverts commit 60f412cd75eb8d931e9e8c351265b0a2a5d81ffa.

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -3,7 +3,6 @@ import userReducer from './userSlice';
 import viewedProfileReducer from './viewedProfileSlice';
 import messagingReducer from './messagingSlice';
 import exchangeModalReducer from './exchangeModalSlice';
-import forumReducer from './forumSlice';
 
 const store = configureStore({
   reducer: {
@@ -11,7 +10,6 @@ const store = configureStore({
     viewedProfile: viewedProfileReducer,
     messaging: messagingReducer,
     exchangeModal: exchangeModalReducer,
-    forum: forumReducer,
   },
 });
 
